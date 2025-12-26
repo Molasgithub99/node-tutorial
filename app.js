e8044fcd30d35c748c12f507aa4c32a12282a846
Molasgithub99 <modoulaminmanneh98@gmail.com>
2025-12-26 04:28:36 -0700

    second commit

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/') {
-//         return res.end('Welcome to our home page!');
-//     }
-//     if (req.url === '/about') {
-//         return res.end('Here is our short history');
-//     }
-//     res.end(`
-//         <h1>Oops!</h1>
-//         <p>We can't seem to find the page you are looking for</p>
-//         <a href="/">back home</a>
-//     `);
-// });
+const server = http.createServer((req, res) => {
+    
+});
 
-// server.listen(5000);
-
-const _ = require('lodash');
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-
+server.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+});
